0f8875441ccf92d89241e2b1a51ec66ad502c044
chickenjunjj <chickenjunjj@gmail.com>
2025-05-27 15:51:26 +0900

    Added db and auth

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBOOG7jSyKZHCxxd2MtulQ6eK1mpaA8Nko",
@@ -11,3 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export default { db, auth };
